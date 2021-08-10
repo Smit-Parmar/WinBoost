b2f821f1333e5a30c53e0c84a701cf150e79a6a8
Smit Parmar <61695474+cyborg7898@users.noreply.github.com>
2021-08-10 15:31:16 +0530

    vesion 2.1

--- winboost/__init__.py
+++ winboost/__init__.py
@@ -1,16 +1,31 @@
 import win32com.shell.shell as shell
 import subprocess
-
+import os
+import time
 
 
 def go():
-    try:
-        pObj = subprocess.Popen('del /q/f/s %TEMP%\*', shell=True, stdout = subprocess.PIPE, stderr= subprocess.PIPE)
-        rTup = pObj.communicate()
-        rCod = pObj.returncode
+    print(""" 
 
+█▀ █▀▀ ▄▀█ █▄░█ █▄░█ █ █▄░█ █▀▀ ░ ░ ░
+▄█ █▄▄ █▀█ █░▀█ █░▀█ █ █░▀█ █▄█ ▄ ▄ ▄
+            """)
+    time.sleep(4)
+    try:
         commands = 'del C:\Windows\Prefetch\*.* /Q'
+
         shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c '+commands)
+        print(""" 
+
+█▀█ █▀█ ▀█▀ █ █▀▄▀█ █ ▀█ █ █▄░█ █▀▀   █▀ █▄█ █▀ ▀█▀ █▀▀ █▀▄▀█ ░ ░ ░
+█▄█ █▀▀ ░█░ █ █░▀░█ █ █▄ █ █░▀█ █▄█   ▄█ ░█░ ▄█ ░█░ ██▄ █░▀░█ ▄ ▄ ▄
+            """)
+        time.sleep(3)
+        #For clear TEMP
+        os.system("del /q/f/s %TEMP%\*")
+
+        #For clear TEMP
+        os.system("del /q/f/s TEMP\*")
 
         print(""" 
 
@@ -23,5 +38,5 @@ def go():
             """)
 
         
-    except:
+    except :
         pass
